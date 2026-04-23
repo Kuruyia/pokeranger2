617875a68e76f06aa633907b230671a178b8195b
Kuruyia <github@kuruyia.net>
2026-04-23 23:13:07 +0200

    ci: send Discord message
    This updates the CI to send a message to a thread on the pret Discord
    server with the results of calcrom for pushes to the main branch.
    
    Signed-off-by: Kuruyia <github@kuruyia.net>

--- .github/calcrom/calcrom.sh
+++ .github/calcrom/calcrom.sh
@@ -2,14 +2,21 @@
 
 set -euxo pipefail
 
-# Only run this script if it's the main branch build.
-if [[ "$GITHUB_REF" != "refs/heads/main" || "$GITHUB_EVENT_NAME" != "push" ]]; then
-  exit 0
-fi
-
 url="$1"
 build_name="ranger2.us"
 build_dir="$2"
 
-output=$("${GITHUB_WORKSPACE}"/.github/calcrom/calcrom.py "$build_dir" | sed -E ':a;N;$!ba;s/\r{0,1}\n/\\n/g')
+output=$("${GITHUB_WORKSPACE}"/.github/calcrom/calcrom.py "$build_dir")
+
+echo '```' >> $GITHUB_STEP_SUMMARY
+echo "$output" >> $GITHUB_STEP_SUMMARY
+echo '```' >> $GITHUB_STEP_SUMMARY
+
+# Only send the message if it's the main branch build.
+if [[ "$GITHUB_REF" != "refs/heads/main" || "$GITHUB_EVENT_NAME" != "push" ]]; then
+  exit 0
+fi
+
+output=$(echo "$output" | sed -E ':a;N;$!ba;s/\r{0,1}\n/\\n/g')
+
 curl -d "{\"username\": \"$CALCROM_DISCORD_WEBHOOK_USERNAME\", \"avatar_url\": \"$CALCROM_DISCORD_WEBHOOK_AVATAR_URL\", \"content\":\"\`\`\`$build_name progress:\\n$output\`\`\`\"}" -H "Content-Type: application/json" -X POST "$url"
